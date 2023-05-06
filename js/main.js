f9c09de48dc41145339723ccaee95835f34bc9b5
Dev-Abdalla-Magdy <you@example.com>
2023-05-06 19:21:37 +0300

    XO_Game_0.2v

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@
 
 let turn = 'x';
 let head = document.querySelector('.title'); 
+let box = document.querySelector('.box'); 
 let square = [];
 
 function gameOver(){
-    alert("The Game Over");
+    setTimeout(() => {
+        alert("The Game Over");
+    }, 500);
 }
 
 function end(num1,num2,num3){
     head.innerHTML = `<span>${square[num1]}</span> The Winner`;
     head.style.textTransform = `uppercase`;
-    document.getElementById('item' + num1).style.backgroundColor = "#01fe21";
-    document.getElementById('item' + num2).style.backgroundColor = "#01fe21";
-    document.getElementById('item' + num3).style.backgroundColor = "#01fe21";
+    document.getElementById('item' + num1).style.backgroundColor = "#9a0076";
+    document.getElementById('item' + num2).style.backgroundColor = "#9a0076";
+    document.getElementById('item' + num3).style.backgroundColor = "#9a0076";
     setInterval(function(){head.innerHTML += '.'},1000);
     setTimeout(function(){location.reload()},3000);
 }
@@ -25,58 +28,47 @@ function winner(){
     /* Start Rows */
     if(square[1] === square[2] && square[2] === square[3] && square[1] !== ''){
         end(1,2,3);
-        setTimeout(() => {
-            gameOver();
-        }, 500);
+        gameOver();
     }
     else if(square[4] === square[5] && square[5] === square[6] && square[4] !== ''){
         end(4,5,6);
-        setTimeout(() => {
-            gameOver();
-        }, 500);
+        gameOver();
     }
     else if(square[7] === square[8] && square[8] === square[9] && square[7] !== ''){
         end(7,8,9);
-        setTimeout(() => {
-            gameOver();
-        }, 500);
+        gameOver();
     }
     /* End Rows */
     /* Start Columns */
     else if(square[1] === square[4] && square[4] === square[7] && square[1] !== ''){
         end(1,4,7);
-        setTimeout(() => {
-            gameOver();
-        }, 500);
+        gameOver();
     }
     else if(square[2] === square[5] && square[5] === square[8] && square[2] !== ''){
         end(2,5,8);
-        setTimeout(() => {
-            gameOver();
-        }, 500);
+        gameOver();
     }
     else if(square[3] === square[6] && square[6] === square[9] && square[3] !== ''){
         end(3,6,9);
-        setTimeout(() => {
-            gameOver();
-        }, 500);
+        gameOver();
     }
     /* End Columns */
     /* Start Cross */
     else if(square[1] === square[5] && square[5] === square[9] && square[1] !== ''){
         end(1,5,9);
-        setTimeout(() => {
-            gameOver();
-        }, 500);
+        gameOver();
     }
     else if(square[3] === square[5] && square[5] === square[7] && square[3] !== ''){
         end(3,5,7);
-        setTimeout(() => {
-            gameOver();
-        }, 500);
+        gameOver();
     }
     /* End Cross */
     /* Start Draw */
+    else if (((square[1]&&square[2]&&square[3]&&square[4]&&square[5]&&square[6]&&square[7]&&square[8]&&square[9]) !== '' ) && turn === 'o'){
+        head.innerHTML = `OOPS! It's Draw`;
+        setInterval(function(){head.innerHTML += '.'},1000);
+        setTimeout(function(){location.reload()},3000);
+    }
     /* End Draw */
 }
 
